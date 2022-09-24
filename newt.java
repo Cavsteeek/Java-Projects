44525e67276a390c255260ebc2de73256c3abbd1
Cavsteeek <107413476+Cavsteeek@users.noreply.github.com>
2022-09-24 22:50:26 +0100

    Update newt.java

--- newt.java
+++ newt.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 public class newt{
-
-  
     public static void main(String[] args){
-         String[] name = new String[5];
+        String[] name = new String[5];
         int[] matNO = new int[5];
         Scanner inu = new Scanner(System.in);
         System.out.println("Students and Their Matric Number");
         System.out.println("Input your matric number and name");
-        for(int i=0;i<5;i++){
+      //Code block for accepting user input  
+      for(int i=0;i<5;i++){
         System.out.print("Matric Number: ");
         matNO[i]=inu.nextInt();
         inu.nextLine();
@@ -17,6 +16,7 @@ public class newt{
         name[i]=inu.nextLine();
         System.out.println();
         }
+      //Code block for the output of the user input
         for(int j=0;j<5;j++){
             System.out.print("Student ID");
             System.out.print(" "+j+1);
@@ -25,8 +25,5 @@ public class newt{
             System.out.print(", "+matNO[j]);
             System.out.println();
         }
-       
-
-    
     }
 }
