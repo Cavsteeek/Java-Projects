3b71df2b41a4df6461dd86495d35dcc51bc31518
Cavsteeek <107413476+Cavsteeek@users.noreply.github.com>
2022-09-24 22:44:45 +0100

    Add files via upload

---
+++ newt.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class newt{
+
+  
+    public static void main(String[] args){
+         String[] name = new String[5];
+        int[] matNO = new int[5];
+        Scanner inu = new Scanner(System.in);
+        System.out.println("Students and Their Matric Number");
+        System.out.println("Input your matric number and name");
+        for(int i=0;i<5;i++){
+        System.out.print("Matric Number: ");
+        matNO[i]=inu.nextInt();
+        inu.nextLine();
+        System.out.println();
+        System.out.print("Full Name: ");
+        name[i]=inu.nextLine();
+        System.out.println();
+        }
+        for(int j=0;j<5;j++){
+            System.out.print("Student ID");
+            System.out.print(" "+j+1);
+            System.out.print(": ");
+            System.out.print(" "+name[j]);
+            System.out.print(", "+matNO[j]);
+            System.out.println();
+        }
+       
+
+    
+    }
+}
